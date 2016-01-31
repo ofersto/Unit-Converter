cd6453640b8c6f84bf3087855cc3a722c985bd9a
Ofer Stolov <ofersto123@gmail.com>
2016-01-31 17:29:10 +0200

    Saving data to localStorage

--- js/converter.coffee
+++ js/converter.coffee
@@ -1,60 +1,86 @@
-$ ->
-	$("select").material_select()
-	$("select[name='type']").on "change", ->
-		$("select").material_select()
+storage = localStorage
+
+getKeyByValue = (obj, value) ->
+	for key in Object.keys(obj)
+		if obj[key] == value
+			return key
+	undefined
 
 app = angular.module "unitConverter", []
 app.controller "mainController", ($scope) ->
-	$scope.units =
-		"Distance":
-			"Kilometer": 1000
-			"Meter": 1
-			"Centimeter": 0.01
-			"Milimeter": 0.001
-			"Mile": 1609.34
-			"Inch": 0.0254
-			"Yard": 0.9144
-			"Foot": 0.3048
-			"Nautical Mile": 1852
+	if storage.units
+		units = storage.units
+		$scope.units = JSON.parse units
+	else
+		$scope.units =
+			"Distance":
+				"Kilometer": 1000
+				"Meter": 1
+				"Centimeter": 0.01
+				"Milimeter": 0.001
+				"Mile": 1609.34
+				"Inch": 0.0254
+				"Yard": 0.9144
+				"Foot": 0.3048
+				"Nautical Mile": 1852
 
-		"Mass": 
-			"Ton": 1000000
-			"Kilogram": 1000
-			"Gram": 1
-			"Miligram": 0.001
-			"US ton": 907185
-			"Stone": 6350.29
-			"Pound": 453.592
-			"Ounce": 28.3495
-	$scope.value = 1
-	$scope.from = "Mile"
-	$scope.to = "Kilometer"
-	$scope.type = "Distance"
-	# $.ajax
-	# 	url: "http://api.fixer.io/latest"
-	# 	type: "GET"
-	# 	dataType: "jsonp"
-	# 	success: (data) ->
-	# 		obj = data.rates
-	# 		obj[data.base] = 1
-	# 		Object.keys(obj).sort()
-	# 		$scope.units["Currency"] = obj
-	# 		$scope.type = "Currency"
-	# 		$scope.typeChanged()
-	# 		$("select[name='type']").trigger("change")
-	# 		undefined
-	# 	error: ->
-	# 		$scope.currStatus = "Sorry, but we couldn't get the currencies data"
-	$.getJSON "http://api.fixer.io/latest", (data) ->
-		obj = data.rates
-		obj[data.base] = 1
-		Object.keys(obj).sort()
-		$scope.units["Currency"] = obj
-		$scope.type = "Currency"
-		$scope.typeChanged()
-		$("select[name='type']").trigger("change")
-	.fail ->
-		$scope.currStatus = "Sorry, but we couldn't get the currencies data"
+			"Mass": 
+				"Ton": 1000000
+				"Kilogram": 1000
+				"Gram": 1
+				"Miligram": 0.001
+				"US ton": 907185
+				"Stone": 6350.29
+				"Pound": 453.592
+				"Ounce": 28.3495
+		storage.units = JSON.stringify($scope.units)
+	if storage.value
+		$scope.value = Number(storage.value)
+	else
+		$scope.value = 1
+
+	if storage.type
+		$scope.type = storage.type
+	else
+		$scope.type = "Distance"
+
+	if storage.from
+		$scope.from = storage.from
+	else
+		$scope.from = Object.keys($scope.units[$scope.type])[0]
+
+	if storage.to
+		$scope.to = storage.to
+	else
+		$scope.to = Object.keys($scope.units[$scope.type])[1]
+
+	getCurrData = ->
+		now = new Date()
+		d = now.getDate()
+		m = now.getMonth() + 1
+		y = now.getFullYear()
+		if d < 10
+			d = '0' + d
+		if m < 10
+			m = '0' + m
+		nowStr = y + "-" + m + "-" + d
+		if storage.currDate != nowStr
+			console.log "get currencies data"
+			$.getJSON "http://api.fixer.io/latest", (data) ->
+				if not data.date == nowStr
+					obj = data.rates
+					obj[data.base] = 1
+					Object.keys(obj).sort()
+					$scope.units["Currency"] = obj
+					storage.units = JSON.stringify $scope.units
+					storage.currDate = data.date
+					$("select[name='type']").trigger("change")
+				$scope.currStatus = "Currencies data are loaded successfully"
+			.fail ->
+				$scope.currStatus = "Sorry, but we couldn't get the currencies data"
+		else
+			$scope.currStatus = "The currencies data is already loaded!"
+	getCurrData()
 	$scope.Utils = 
 		keys: Object.keys
 		round: (n, dec) ->
@@ -63,6 +89,7 @@ app.controller "mainController", ($scope) ->
 			else
 				$scope.convert()
 	$scope.typeChanged = ->
+		storage.type = $scope.type
 		$scope.value = 1
 		$scope.from = Object.keys($scope.units[$scope.type])[0]
 		$scope.to = Object.keys($scope.units[$scope.type])[1]
@@ -71,6 +98,9 @@ app.controller "mainController", ($scope) ->
 	$scope.convert = ->
 		from = $scope.units[$scope.type][$scope.from]
 		to = $scope.units[$scope.type][$scope.to]
+		storage.value = $scope.value
+		storage.from = getKeyByValue($scope.units[$scope.type], from)
+		storage.to = getKeyByValue($scope.units[$scope.type], to)
 		if $scope.type != "Currency"
 			result = Number($scope.value) * from / to
 		else
@@ -78,3 +108,8 @@ app.controller "mainController", ($scope) ->
 		$scope.result = result
 		undefined
 	undefined
+
+$ ->
+	$("select").material_select()
+	$("select[name='type']").on "change", ->
+		$("select").material_select()
--- js/converter.js
+++ js/converter.js
@@ -1,55 +1,108 @@
 (function() {
-  var app;
+  var app, getKeyByValue, storage;
 
-  $(function() {
-    $("select").material_select();
-    return $("select[name='type']").on("change", function() {
-      return $("select").material_select();
-    });
-  });
+  storage = localStorage;
+
+  getKeyByValue = function(obj, value) {
+    var i, key, len, ref;
+    ref = Object.keys(obj);
+    for (i = 0, len = ref.length; i < len; i++) {
+      key = ref[i];
+      if (obj[key] === value) {
+        return key;
+      }
+    }
+    return void 0;
+  };
 
   app = angular.module("unitConverter", []);
 
   app.controller("mainController", function($scope) {
-    $scope.units = {
-      "Distance": {
-        "Kilometer": 1000,
-        "Meter": 1,
-        "Centimeter": 0.01,
-        "Milimeter": 0.001,
-        "Mile": 1609.34,
-        "Inch": 0.0254,
-        "Yard": 0.9144,
-        "Foot": 0.3048,
-        "Nautical Mile": 1852
-      },
-      "Mass": {
-        "Ton": 1000000,
-        "Kilogram": 1000,
-        "Gram": 1,
-        "Miligram": 0.001,
-        "US ton": 907185,
-        "Stone": 6350.29,
-        "Pound": 453.592,
-        "Ounce": 28.3495
+    var getCurrData, units;
+    if (storage.units) {
+      units = storage.units;
+      $scope.units = JSON.parse(units);
+    } else {
+      $scope.units = {
+        "Distance": {
+          "Kilometer": 1000,
+          "Meter": 1,
+          "Centimeter": 0.01,
+          "Milimeter": 0.001,
+          "Mile": 1609.34,
+          "Inch": 0.0254,
+          "Yard": 0.9144,
+          "Foot": 0.3048,
+          "Nautical Mile": 1852
+        },
+        "Mass": {
+          "Ton": 1000000,
+          "Kilogram": 1000,
+          "Gram": 1,
+          "Miligram": 0.001,
+          "US ton": 907185,
+          "Stone": 6350.29,
+          "Pound": 453.592,
+          "Ounce": 28.3495
+        }
+      };
+      storage.units = JSON.stringify($scope.units);
+    }
+    if (storage.value) {
+      $scope.value = Number(storage.value);
+    } else {
+      $scope.value = 1;
+    }
+    if (storage.type) {
+      $scope.type = storage.type;
+    } else {
+      $scope.type = "Distance";
+    }
+    if (storage.from) {
+      $scope.from = storage.from;
+    } else {
+      $scope.from = Object.keys($scope.units[$scope.type])[0];
+    }
+    if (storage.to) {
+      $scope.to = storage.to;
+    } else {
+      $scope.to = Object.keys($scope.units[$scope.type])[1];
+    }
+    getCurrData = function() {
+      var d, m, now, nowStr, y;
+      now = new Date();
+      d = now.getDate();
+      m = now.getMonth() + 1;
+      y = now.getFullYear();
+      if (d < 10) {
+        d = '0' + d;
+      }
+      if (m < 10) {
+        m = '0' + m;
+      }
+      nowStr = y + "-" + m + "-" + d;
+      if (storage.currDate !== nowStr) {
+        console.log("get currencies data");
+        return $.getJSON("http://api.fixer.io/latest", function(data) {
+          var obj;
+          if (!data.date === nowStr) {
+            obj = data.rates;
+            obj[data.base] = 1;
+            Object.keys(obj).sort();
+            $scope.units["Currency"] = obj;
+            storage.units = JSON.stringify($scope.units);
+            storage.currDate = data.date;
+            $("select[name='type']").trigger("change");
+          }
+          return $scope.currStatus = "Currencies data are loaded successfully";
+        }).fail(function() {
+          return $scope.currStatus = "Sorry, but we couldn't get the currencies data";
+        });
+      } else {
+        return $scope.currStatus = "The currencies data is already loaded!";
       }
     };
-    $scope.value = 1;
-    $scope.from = "Mile";
-    $scope.to = "Kilometer";
-    $scope.type = "Distance";
-    $.getJSON("http://api.fixer.io/latest", function(data) {
-      var obj;
-      obj = data.rates;
-      obj[data.base] = 1;
-      Object.keys(obj).sort();
-      $scope.units["Currency"] = obj;
-      $scope.type = "Currency";
-      $scope.typeChanged();
-      return $("select[name='type']").trigger("change");
-    }).fail(function() {
-      return $scope.currStatus = "Sorry, but we couldn't get the currencies data";
-    });
+    getCurrData();
     $scope.Utils = {
       keys: Object.keys,
       round: function(n, dec) {
@@ -61,6 +114,7 @@
       }
     };
     $scope.typeChanged = function() {
+      storage.type = $scope.type;
       $scope.value = 1;
       $scope.from = Object.keys($scope.units[$scope.type])[0];
       $scope.to = Object.keys($scope.units[$scope.type])[1];
@@ -71,6 +125,9 @@
       var from, result, to;
       from = $scope.units[$scope.type][$scope.from];
       to = $scope.units[$scope.type][$scope.to];
+      storage.value = $scope.value;
+      storage.from = getKeyByValue($scope.units[$scope.type], from);
+      storage.to = getKeyByValue($scope.units[$scope.type], to);
       if ($scope.type !== "Currency") {
         result = Number($scope.value) * from / to;
       } else {
@@ -80,6 +137,13 @@
       return void 0;
     };
     return void 0;
+  });
+
+  $(function() {
+    $("select").material_select();
+    return $("select[name='type']").on("change", function() {
+      return $("select").material_select();
+    });
   });
 
 }).call(this);
